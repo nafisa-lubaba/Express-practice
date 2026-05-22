630efed406a92065258612737a7bee1d4da210e0
nafisa-lubaba <nafisalubaba71@gmail.com>
2026-05-22 13:47:40 +0600

    learning

---
+++ src/db/index.ts
@@ -0,0 +1,25 @@
+import { Pool } from "pg";
+import config from "../config";
+
+export const pool = new Pool({
+  connectionString:  config.connection_string
+})
+
+export const initDB = async () => {
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        is_active BOOLEAN DEFAULT true,
+        age INTEGER,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+      )`);
+    console.log("Table created successfully");
+  } catch (err) {
+    console.error("Error connecting to the database:", err);
+  }
+}
