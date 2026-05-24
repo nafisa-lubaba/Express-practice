2f593aaadbc83a190fcc764298d5c228dc69f678
nafisa-lubaba <nafisalubaba71@gmail.com>
2026-05-24 15:00:54 +0600

    jwt

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import config from './config'
 import { initDB, pool } from './db'
 import { userRoute } from './modules/user/user.route'
 import { profileRoute } from './modules/profile/profile.route'
+import { authRoute } from './modules/auth/auth.route'
 const app: Application = express()
 app.use(express.json())
 app.use(express.text())
